550f5154957d5ebefde917543bf17f9260ae90ec
Piotr-Sicinski <piotr.sicinski2002@gmail.com>
2023-10-01 11:56:18 +0200

    Final commit hackathon

--- README.md
+++ README.md
@@ -13,6 +13,20 @@ in the .env file as follows:
 IBM_TOKEN="your_token"
 ```
 
+## Generate art 
+Run following command to execute program with random initial pattern. Result will be saved to quantum.gif :
+```
+cd game
+python main.py
+```
+
+
+You can use `-l` flag to load initial pattern from pattern.png. You can edit it in external program (e.g. MS Paint).
+White pixel = dead cell, any other pixel = living cell:
+```
+python main.py -l
+```
+
 ## Testing 
 Run following command to execute test suite:
 ```
--- game/main.py
+++ game/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 from utils import digital_alive, quantum_alive
 from PIL import Image
+import argparse
 
 SIZE = 32
 
@@ -10,6 +11,14 @@ COLOR = [255, 0, 255]
 def initialize_board(n):
     """Initialize the Game of Life board with random states."""
     return np.random.choice([0, 1], size=(n, n))
+
+
+def initialize_board_from_img(img):
+    """Initialize the Game of Life board with pattern from file."""
+    img_array = np.array(img)
+    board = np.where(np.all(img_array[:, :, :3] != [
+                     255, 255, 255], axis=2), 1, 0)
+    return board
 
 
 def evolve(board, alive_fn):
@@ -54,9 +63,38 @@ def save_to_gif(frames, name, fps):
 
 
 if __name__ == "__main__":
-    board = initialize_board(SIZE)
+
+    parser = argparse.ArgumentParser(description='Load starting pattern')
+    parser.add_argument('-l', '--load',
+                        action='store_true',
+                        help='Load starting pattern from pattern.png file instead of random patern')
+    params = parser.parse_args()
+
+    if params.load:
+        img = Image.open("pattern.png")
+
+        if img.size != (SIZE, SIZE):
+            print(
+                f"pattern.png has size different then {SIZE},{SIZE}! Using random pattern.")
+            params.load = False
+        else:
+            print("Starting pattern loaded!")
+
+        board = initialize_board_from_img(img)
+
+    if not params.load:
+        board = initialize_board(SIZE)
+
+    print("Art is being created ... ")
+
     frames = play(board, digital_alive, steps=100)
     save_to_gif(frames, "digital.gif", fps=10)
-    qboard = initialize_board(SIZE)
-    frames = play(qboard, quantum_alive, steps=20)
+
+    if params.load:
+        board = initialize_board_from_img(img)
+    else:
+        board = initialize_board(SIZE)
+    frames = play(board, quantum_alive, steps=20)
     save_to_gif(frames, "quantum.gif", fps=10)
+
+    print("Your art is ready!  ... ")
